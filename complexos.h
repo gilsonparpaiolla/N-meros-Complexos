1f7c445734499b0d29be0190f7172d72b115a676
GilsonLuizLuiz <82735418+gilsonparpaiolla@users.noreply.github.com>
2021-09-25 21:00:48 +0000

    Initial commit

---
+++ complexos.h
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+
+class Complexos
+{
+  private:
+
+    float Soma;
+    float Real1, Real2;
+    float Imaginario1, Imaginario2;
+    float MenosR, MaisR, MenosI, MaisI;
+    float DivdCR, DivdCI;
+    float M1, M2,M3,M4;
+    float MultCR, MultCI;
+
+  public:
+
+    Complexos(float R1, float R2, float I1, float I2)
+    {
+        Real1 = R1;
+        Real2 = R2;
+        Imaginario1 = I1;
+        Imaginario2 = I2;
+    }
+
+    Complexos(float R1, float R2, float I1, float I2, float S)
+    {
+        Real1 = R1;
+        Real2 = R2;
+        Imaginario1 = I1;
+        Imaginario2 = I2;
+        Soma = S;
+    }
+
+    float soma_numero(float R1, float R2, float I1, float I2)
+    {
+      MaisR = R1 + R2;
+      MaisI = I1 + I2;
+
+      cout << "\nA soma dos numeros complexos e: " << MaisR;
+
+      if(MaisI < 0)
+      {
+        cout << " - " << MaisI << "j";
+      }else
+      cout << " + " << MaisI << "j";
+
+      return(MaisR);
+      return(MaisI);
+    }
+
+    float subtrai_numero (float R1, float R2, float I1, float I2)
+    {
+      MenosR = R1 - R2;
+      MenosI = I1 - I2;
+
+      cout << "\nA subtração dos numeros complexos e: " << MenosR;
+
+      if(MenosI < 0)
+      {
+        cout << " - " << MenosI << "j";
+      }else
+      cout << " + " << MenosI << "j";
+    }
+
+    void multiplica_numero (float R1, float R2, float I1, float I2)
+    {
+      MultCR = (R1*R2) - (I1*I2);
+      MultCI = (I2*R1) + (I1*R2);
+
+    cout << "\nA multiplicacao dos numeros complexos e: " << MultCR;
+
+      if(MultCI < 0)
+      {
+        cout << " - " << MultCI << "j";
+      }else
+      cout << " + " << MultCI << "j";
+
+    }
+
+    void divide_numero (float R1, float R2, float I1, float I2)
+    {
+      float denominador;
+
+
+      DivdCR = (R1*R2) + (I1*I2);
+
+      DivdCI = (I1*R2) - (I2*R1);
+
+      denominador = (R2*R2) + (I2*I2);
+
+      cout << "\n\n   " << DivdCR << "\t" << DivdCI << endl;
+      if(DivdCI > 0)
+        cout << ("------ + ------- j\n");
+      else
+        cout << ("------  ------- j\n");
+      cout << "  " << denominador << "\t  " << denominador << "\n\n";
+    }
+
+    void modulo (float R1, float R2, float I1, float I2)
+    {
+      M1 = sqrt(pow(R1,2) + pow(I1,2));
+      M2 = sqrt(pow(R2,2) + pow(I2,2));
+
+      cout << "\nO modulo do numero complexo: " << R1;
+      if(I1>0)
+        {
+          cout << " + " << I1 << "j e: " << M1;
+        }
+        else
+            cout << I1 << "j e: " << M1;
+
+        cout << "\nO modulo do numero complexo: " << R2;
+          if(I2>0)
+            {
+              cout << " + " << I2 << "j e: " << M2;
+            }
+            else
+                cout << I2 << "j e: " << M2;
+     }
+
+    static float Soma_C(float A, float b)
+    {
+        float Soma;
+        Soma = A + b;
+        return(Soma);
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include "Complexos.h"
+#include <cstdlib>
+
+using namespace std;
+
+int main  ()
+{
+    int i, operacao, c=1, escolha,q;
+    float R1, R2, I1, I2, R3, R4, I3, I4, S1, S2;
+
+    cout << ("Tecle 1 para iniciar") << endl;
+    cin >> q;
+    system("cls");
+
+    Complexos operacoes(R1, R2, I1, I2);
+    Complexos pacotes_NC(R1, R2, I1, I2,0);
+
+
+
+    while(q==1)
+    {
+
+        for(int i=0; i<2; i++)
+        {
+            if(i==0)
+            {
+                cout << "----------------- Calculadora de Numeros Complexos --------------------" << endl;
+
+
+                cout << "Digite o numero complexo, informando primeiro a parte REAL e depois a parte IMAGINARIA" << endl;
+
+                cin >> R1;
+                cin >> I1;
+
+                if(I1>0)
+                    cout << R1 << " + " << I1 << "j";
+                else
+                    cout << R1 << I1 << "j";
+
+
+                cout << ("\n\nDigite o numero complexo, informando primeiro a parte REAL e depois a parte IMAGINARIA") << endl;
+                cin >> R2;
+                cin >> I2;
+
+
+                if(I2>0)
+                    cout << R2 << " + " << I2 << "j";
+                else
+                    cout << R2 << I2 << "j";
+
+            }else
+            {
+                system("cls");
+                cout << "----------------- Calculadora de Numeros Complexos --------------------" << endl;
+                cout<< "---------------Digite o Segundo Par de Numeros Complexos------------" << endl;
+
+
+                cout << "\nDigite o numero complexo, informando primeiro a parte REAL e depois a parte IMAGINARIA" << endl;
+
+                cin >> R3;
+                cin >> I3;
+
+                if(I3>0)
+                    cout << R3 << " + " << I3 << "j";
+                else
+                    cout << R3 << I3 << "j";
+
+
+                cout << ("\n\nDigite o numero complexo, informando primeiro a parte REAL e depois a parte IMAGINARIA") << endl;
+                cin >> R4;
+                cin >> I4;
+
+
+                if(I4>0)
+                    cout << R4 << " + " << I4 << "j";
+                else
+                    cout << R4 << I4 << "j";
+
+            }
+        }
+
+
+        while (c == 1)
+        {
+
+            cout << "\n\n---------- Realizar uma operação básica ----------"  << endl;
+
+            cout << "\n\nOPERAÇÕES COM NÚMEROS COMPLEXOS:" << endl;
+            cout << "1: Soma" << endl;
+            cout << "2: Subtração" << endl;
+            cout << "3: Multiplicação" << endl;
+            cout << "4: Divisão" << endl;
+            cout << "5: Modulo" << endl;
+
+            cin >> (operacao);
+
+            if (operacao == 1)
+            {
+              S1 = operacoes.soma_numero(R1, R2, I1, I2);
+              S2 =  pacotes_NC.soma_numero(R3, R4, I3, I4);
+            }
+            else if (operacao == 2)
+            {
+              operacoes.subtrai_numero(R1, R2, I1, I2);
+              pacotes_NC.subtrai_numero(R3, R4, I3, I4);
+            }
+            else if (operacao == 3)
+            {
+              operacoes.multiplica_numero(R1, R2, I1, I2);
+              pacotes_NC.multiplica_numero(R3, R4, I3, I4);
+            }
+            else if (operacao == 4)
+            {
+              operacoes.divide_numero(R1, R2, I1, I2);
+              pacotes_NC.divide_numero(R3, R4, I3, I4);
+            }
+            else if (operacao == 5)
+            {
+              operacoes.modulo(R1, R2, I1, I2);
+              pacotes_NC.modulo(R3, R4, I3, I4);
+            }
+
+
+        cout << ("\n\n\nTecle 1 para repetir ou 0 para sair: ");
+        cin >> c;
+        system("cls");
+        }
+
+
+        cout << "A soma dos 2 objetos e: ";
+        cout << Complexos::Soma_C(S1,S2);
+
+
+        cout << "\n\n\n\n\nDeseja escolher outros valores dos numeros, 1 para escolher ou 0 para sair: ";
+        cin >> q;
+        c=1;
+        system("cls");
+    }
+  exit(0);
+}
